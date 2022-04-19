5c6c11743dc0b5dd7cd3a290096300b80016186b
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2022-04-19 21:46:09 +0900

    greedy
    ddd

---
+++ test.js
@@ -0,0 +1,31 @@
+function solution(n,lost,reserve){
+    //reserve들은 lost에 포함되어도 무조건 체육복을 입을 수 있다.
+    //lost에 있는 reserve는 체육복을 빌려줄 수 없다.
+    lost.sort();
+    reserve.sort();
+    //1.reserve에서 lost 서로 중복되는 값 삭제하기
+    let temp_reserve = reserve.filter((it)=>!lost.includes(it));
+    let temp_lost = lost.filter((it)=>!reserve.includes(it));
+    reserve=temp_reserve;
+    lost=temp_lost;
+    console.log("prev:"+reserve);
+    console.log("prev:"+lost);
+
+
+    //2.reserve와 lost를 비교해서 lost에서 첨삭 가능한 부분 삭제
+    for(var i=0;i<reserve.length;i++){
+        for(var j=0;j<lost.length;j++){
+            if(reserve[i]-1==lost[j]){ //왼쪽 친구 빌려줬을때
+                reserve.splice(i,1);
+                lost.splice(j,1)
+                i--;
+            }
+            else if(reserve[i]+1==lost[j]){ //오른쪽 친구 빌려줬을때
+                reserve.splice(i,1);
+                lost.splice(j,1);
+                i--;
+            }
+        }
+    }
+    return n-lost.length;
+}
