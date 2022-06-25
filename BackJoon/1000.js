17f1b47281d145fe0f634fb340bd5c8e0c480fdd
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2022-06-25 14:14:56 +0900

    dddd
    ddddd

---
+++ BackJoon/1000.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+var input = fs.readFileSync('C:/Users/mspc/Documents/GitHub/algorithm/BackJoon/test.txt').toString().trim().split('');
+var a=parseInt(input[0]);
+var b=parseInt(input[1]);
+console.log(a+b);
+
+5163
--- BackJoon/1110.js
+++ BackJoon/1110.js
@@ -1,31 +1,29 @@
+const fs = require('fs');
+const input = fs.readFileSync("BackJoon/test.txt").toString().trim();
+
+
 const checknum = (a,b)=>{
     return a==b?true:false;
 }
 
 const newnum = (a)=>{
     var b = parseInt(a/10)+(a%10);
-    console.log("newnumfun:"+b);
-    return ((a%10)*10)+(b%10);
+    return ((a%10)*10)+b;
     
 }
-
-
-let num = 26;
-const correctnum = num;
+let num = Number(input);
 let count = 0;
 
 
 do{
     var num2=newnum(num);
     count++;
-    console.log("1:"+num);
-    console.log("2:"+num2);
-    if(checknum(correctnum,num2)==true){
+    if(checknum(num,num2)==true){
         break;
     }
     else{
         num=num2;
     }
-}while(count<10)
+}while(true)
 
-
+console.log(count);
