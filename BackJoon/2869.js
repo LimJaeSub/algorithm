68f2acc82ce04a3b8d23429048ffcf847921771f
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2022-06-30 17:16:42 +0900

    Update 2869.js
    dd

--- BackJoon/2869.js
+++ BackJoon/2869.js
@@ -3,11 +3,4 @@ const input = fs.readFileSync("BackJoon/test.txt").toString().trim().split(" ");
 
 const [up,down,tree] = [parseInt(input[0]),parseInt(input[1]),parseInt(input[2])];
 
-let now = 0;
-let day = 0;
-do{
-    if(now>down){
-        now=now-down;
-        now=now+up;
-    }
-}while(now<tree)
+console.log(Math.ceil((tree-down)/(up-down)));
